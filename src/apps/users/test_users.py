f0ca434b0a87f72ca13dd2fbbc4e49d56ab421e3
rafael <rafaondjango@gmail.com>
2024-01-11 14:18:56 +0000

    feat: tests users

--- src/apps/users/test_users.py
+++ src/apps/users/test_users.py
@@ -1,11 +1,27 @@
-from main import app
-from base import client
+from src.base import authenticated_client
+import json
 
 def test_request_retrieve_a_user():
     userd_id_to_retrieve = 1
-    
-    request = client.get(f"/users/{userd_id_to_retrieve}")
-    
-    assert request.status_code == 200
+    response = authenticated_client.get(f"/users/{userd_id_to_retrieve}")
+    assert response.status_code == 200
 
 
+def test_request_list_users():
+    response = authenticated_client.get("/users")
+    assert response.status_code == 200
+
+
+def test_request_create_user():
+    data = {
+        "email": "rafaondjango@gmail.com",
+        "password": "123456"
+    }
+    data = json.dumps(data)
+    response = authenticated_client.post("/users", content=data)
+    user_id = response.json().get("id")
+    
+    response = authenticated_client.delete(f"/users/{user_id}")
+    
+    
+    assert response.status_code == 200
--- src/base.py
+++ src/base.py
@@ -1,24 +1,33 @@
 from fastapi.testclient import TestClient
-from main import app
+from .main import app
+
+unauthenticated_client = TestClient(
+    app,
+    base_url="http://localhost:8000/",
+    raise_server_exceptions=True,
+)
+
 
 def get_headers_with_valid_token() -> dict[str, str]:
     user_data = {
         "username": "admin@mail.com",
         "password": "123456"
     }
-    response = client.post("/auth", data=user_data)
+    response = unauthenticated_client.post("/auth", data=user_data)
     
     token = response.json().get("access_token")
     
     headers = {
-      "Authorization": f"Bearer token"
+      "Authorization": f"Bearer {token}" 
     }
     
     return headers
 
+
 headers = get_headers_with_valid_token()
 
-client = TestClient(
+
+authenticated_client = TestClient(
     app,
     base_url="http://localhost:8000/",
     raise_server_exceptions=True,
