033cd02cb99bc80b24e661e3ad765d97b6ca30e1
rafael <rafaelwildfire@gmail.com>
2024-01-12 00:48:30 +0000

    fix: ajustes nas tasks

--- src/apps/users/tasks.py
+++ src/apps/users/tasks.py
@@ -8,66 +8,48 @@ from fastapi.responses import JSONResponse
 from fastapi import status
 from decouple import config
 
-import os
-import sys
 import asyncio
 
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_path)
 
 rabbit_mq_host = config("RABBIT_MQ_HOST")
 
 
 async def send_email(receiver: str):
     try:
-        me = config("EMAIL_SENDER")
-        you = receiver
-
+        sender = config("EMAIL_SENDER")
         msg = MIMEMultipart("alternative")
         msg["Subject"] = "Olá, confirme o seu cadastro!"
-        msg["From"] = me
-        msg["To"] = you
-        logging.info(project_path)
-        logging.info(os.path.dirname(__file__))
+        msg["From"] = sender
+        msg["To"] = receiver
+        
         HTMLFile = open(
-            "/app/src/apps/users/templates/confirm_register.html", "r")
-
+            "templates/confirm_register.html", "r")
         html = HTMLFile.read()
         html = html.replace(
             "{{button}}",
             f'<a class="button" href="http://localhost:8000/users/confirm-register?email={receiver}">Confirmar cadastro</a>',
         )
         HTMLFile.close()
-
         html_content = MIMEText(html, "html")
-
         msg.attach(html_content)
 
-        logging.info("teste1")
         mail = smtplib.SMTP("smtp.gmail.com", 587)
-        logging.info("teste2")
         mail.ehlo()
-        logging.info("teste3")
         mail.starttls()
-        logging.info("teste4")
-        mail.login(me, config("EMAIL_PASSWORD"))
-        logging.info("teste5")
-        mail.sendmail(me, you, msg.as_string())
-        logging.info("teste6")
+        mail.login(sender, config("EMAIL_PASSWORD"))
+        mail.sendmail(sender, receiver, msg.as_string())
         mail.quit()
-        logging.info("teste7")
     except Exception as e:
-        print(e)
         logging.error(e)
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"detail": "Error while sending email."},
         )
 
-app = Celery('tasks', backend='rpc://',
-             broker=f'pyamqp://guest@{rabbit_mq_host}//')
-app.conf.update(imports=['users.crud'])
 
+app = Celery("tasks", backend="rpc://",
+             broker=f"pyamqp://guest@{rabbit_mq_host}//")
+app.config_from_object("src.apps.users.tasks", namespace="CELERY")
 
 @app.task
 def send_email_celery(receiver: str):
