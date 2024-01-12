e34e3f95ae5020f45a940375956bfa4d86623ffa
rafael <rafaelwildfire@gmail.com>
2024-01-12 01:46:11 +0000

    tests(user): implementando tests para user

--- src/apps/users/test_users.py
+++ src/apps/users/test_users.py
@@ -1,15 +1,8 @@
 from src.base import authenticated_client
 import json
 
-def test_request_retrieve_a_user():
-    userd_id_to_retrieve = 1
-    response = authenticated_client.get(f"/users/{userd_id_to_retrieve}")
-    assert response.status_code == 200
-
-
-def test_request_list_users():
-    response = authenticated_client.get("/users")
-    assert response.status_code == 200
+# ID do usuário de test criado e deletado durante a fase de teste
+user_id = 0
 
 
 def test_request_create_user():
@@ -20,7 +13,36 @@ def test_request_create_user():
     data = json.dumps(data)
     
     response = authenticated_client.post("/users", content=data)
-    user_id = response.json().get("id")
-    response = authenticated_client.delete(f"/users/{user_id}")
     
+    # Alterando a variável global user_id para o usuário de teste ser utilizado nos demais tests
+    global user_id
+    user_id = response.json().get("id")
+    
+    assert response.status_code == 200
+    
+    
+def test_request_retrieve_a_user():
+    userd_id_to_retrieve = user_id
+    response = authenticated_client.get(f"/users/{userd_id_to_retrieve}")
+    assert response.status_code == 200
+
+
+def test_request_list_users():
+    response = authenticated_client.get("/users")
+    assert response.status_code == 200
+
+
+def test_request_update_user():
+    data = {
+        "is_active": True,
+    }
+    data = json.dumps(data)
+    
+    response = authenticated_client.put(f"/users/{user_id}", content=data)
+        
+    assert response.status_code == 200
+
+
+def test_request_delete_user():
+    response = authenticated_client.delete(f"/users/{user_id}")
     assert response.status_code == 200
