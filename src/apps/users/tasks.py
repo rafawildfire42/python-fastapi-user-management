baebd2d68810dbfd7c6588aa69780c5579c44445
rafael <rafaelwildfire@gmail.com>
2024-01-12 01:09:07 +0000

    feat: melhorias no codigo de enviar email

--- src/apps/users/tasks.py
+++ src/apps/users/tasks.py
@@ -1,27 +1,28 @@
+import asyncio
 from celery import Celery
-import smtplib
+from decouple import config
 import logging
+import smtplib
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from fastapi.responses import JSONResponse
 from fastapi import status
-from decouple import config
 
-import asyncio
-
-
+# Pegando referências nas variáveis de ambiente
 rabbit_mq_host = config("RABBIT_MQ_HOST")
+email_password = config("EMAIL_PASSWORD")
+sender = config("EMAIL_SENDER")
 
 
 async def send_email(receiver: str):
     try:
-        sender = config("EMAIL_SENDER")
         msg = MIMEMultipart("alternative")
         msg["Subject"] = "Olá, confirme o seu cadastro!"
         msg["From"] = sender
         msg["To"] = receiver
-        
+
         HTMLFile = open(
             "templates/confirm_register.html", "r")
         html = HTMLFile.read()
@@ -36,7 +37,7 @@ async def send_email(receiver: str):
         mail = smtplib.SMTP("smtp.gmail.com", 587)
         mail.ehlo()
         mail.starttls()
-        mail.login(sender, config("EMAIL_PASSWORD"))
+        mail.login(sender, email_password)
         mail.sendmail(sender, receiver, msg.as_string())
         mail.quit()
     except Exception as e:
@@ -47,9 +48,13 @@ async def send_email(receiver: str):
         )
 
 
-app = Celery("tasks", backend="rpc://",
-             broker=f"pyamqp://guest@{rabbit_mq_host}//")
-app.config_from_object("src.apps.users.tasks", namespace="CELERY")
+app = Celery(
+        main="tasks",
+        backend="rpc://",
+        broker=f"pyamqp://guest@{rabbit_mq_host}//"
+    )
+app.config_from_object("src.apps.users.tasks")
+
 
 @app.task
 def send_email_celery(receiver: str):
