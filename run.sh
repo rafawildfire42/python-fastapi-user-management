c8b25fd55096b50a7140fb899f03bfa3ad28f847
rafael <rafaelwildfire@gmail.com>
2024-01-13 00:27:00 +0000

    fix: deletando arquivo inutilizado

--- run.sh
+++
@@ -1,5 +0,0 @@
-poetry run alembic upgrade head
-export PYTHONPATH=/app/src:$PYTHONPATH
-psql -U admin -d main -h postgres -f populate_database.sql
-poetry run celery --workdir /app/src/apps/users -A tasks worker -l debug &
-poetry run uvicorn src.main:app --host 0.0.0.0 --port 8000
