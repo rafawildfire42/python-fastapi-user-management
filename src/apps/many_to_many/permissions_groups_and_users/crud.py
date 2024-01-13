b2954a3c2c5b4ff685e8fe76856e60422415cd66
rafael <rafaelwildfire@gmail.com>
2024-01-13 12:28:15 +0000

    feat: endpoint criar relacao entre grupo e usuario

--- src/apps/many_to_many/permissions_groups_and_users/crud.py
+++ src/apps/many_to_many/permissions_groups_and_users/crud.py
@@ -1,6 +1,12 @@
+import logging
 from sqlalchemy.orm import Session
 
 from . import models
+from .schemas import UserAndGroupRelation
+from src.apps.permissions_groups.crud import get_permissions_group
+from src.apps.users.crud import get_user_by_id
+
+from fastapi import HTTPException, status
 
 
 def get_user_permissions_group_relation(db: Session, skip: int = 0, limit: int = 100):
@@ -9,3 +15,20 @@ def get_user_permissions_group_relation(db: Session, skip: int = 0, limit: int =
 
 def get_user_permissions_group_relation_filter(db: Session, user_id: int, skip: int = 0, limit: int = 100):
     return db.query(models.permissions_group_user_association).filter_by(user_id=user_id).all()
+
+
+def create_user_permissions_group_relation(db: Session, data: UserAndGroupRelation):
+    permissions_group_id = data.permission_group_id
+    user_id = data.user_id
+    
+    try:
+        db_user = get_user_by_id(db, id=user_id)
+        db_permissions_group = get_permissions_group(db, permissions_group_id=permissions_group_id)
+        db_user.permissions_group.append(db_permissions_group)
+        db.commit()
+        return db.query(models.permissions_group_user_association).filter_by(user_id=1).all()
+    except Exception as e:
+        logging.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Error while creating relation between permissions group #{permissions_group_id} and user #{user_id}. Check if this relation already exists.")
+        
+    
--- src/apps/many_to_many/permissions_groups_and_users/routes.py
+++ src/apps/many_to_many/permissions_groups_and_users/routes.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from .schemas import UserAndGroupRelation
-from src.apps.many_to_many.permissions_groups_and_permissions import crud
+from . import crud
 from src.database.dependencies import get_db
 
 from fastapi import APIRouter, Depends
@@ -21,3 +21,16 @@ def read_users_and_groups(skip: int = 0, limit: int = 100, db: Session = Depends
 
     """
     return crud.get_user_permissions_group_relation(db, skip=skip, limit=limit)
+
+
+@users_and_groups_router.post("/", response_model=UserAndGroupRelation)
+def read_users_and_groups(data: UserAndGroupRelation, db: Session = Depends(get_db)):
+    """
+    List relations between users and gorups
+
+    - **Response:**
+      - All the permissions
+
+    """
+    return crud.create_user_permissions_group_relation(db, data)
+
